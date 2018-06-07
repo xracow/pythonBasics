2b49eb016e14cb58a7bbb6f70a8c168adb4600f6
xracow <xorge.adrian@gmail.com>
2018-06-06 21:01:47 -0500

    example sum two numbers

---
+++ Xorge/p2.py
@@ -0,0 +1,9 @@
+Total=0
+try:
+    while Total< 10:
+        num1=int(input("Escrbe el numero:"))
+        num2=int(input("Escrbe el segundo numero:"))
+        Total=num1+num2
+
+except:
+    print("Es texto")

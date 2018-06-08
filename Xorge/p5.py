acbd157e5c941101d063cfd2865c149ad298257a
xracow <xorge.adrian@gmail.com>
2018-06-07 21:59:45 -0500

    Ejercicio sobre metodos de listas

---
+++ Xorge/p5.py
@@ -0,0 +1,22 @@
+#List
+#Ejemplo para crear litas
+
+
+myList = [2,4,8,16,33,44,66,77]
+
+for n in myList:
+    print(n)
+    
+for n in myList[0:3]:
+    print(n)
+    
+#Ejemplo para iterar
+    
+for n in range(5):
+    print(n)
+    
+#Ejemplo un string se comporta como una lista
+    
+    texto = 'trotamundo' 
+    for n in texto:
+        print(n)
---
+++ Xorge/p6.py
@@ -0,0 +1,24 @@
+'''El usuario tendra que introducir valores numericos, los cuales se almacenarán para después saca
+el promedio y mostrarlo cuando el valor introducido no sea numerico.
+
+5
+7
+8
+s
+promedio: 6.5
+'''
+Flag = 0
+Suma = 0
+ListNum = []
+#print(ListNum[len(ListNum)-1])
+#a=type(ListNum[len(ListNum)-1])
+try:
+    while Flag <= 0:
+        num = int(input('Escribe el valor:'))
+        ListNum.append(num)
+    
+except:
+    for n in ListNum:
+        Suma += n
+        Promedio = Suma / (len(ListNum))
+    print(Promedio)
